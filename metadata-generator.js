0e0f418bbb2a09f48c01a0ca7ce0471be0ce2fe9
MoneshROR <monesh.thoughtwin@gmail.com>
2022-03-21 19:38:46 +0530

    add new metadata field

--- metadata-generator.js
+++ metadata-generator.js
@@ -26,11 +26,20 @@ const imageDir = fs.readdirSync("./assets");imageDir.forEach(img => {
       name: `Image ${img.split(".")[0]}`,
       description: "Iceat NFT collection",
       symbol: "IC",
+      seller_fee_basis_points: 500,
       image: img,
+      attributes: [
+        {"trait_type": "Layer-1", "value": "0"},
+        {"trait_type": "Layer-2", "value": "0"},
+        {"trait_type": "Layer-3", "value": "0"},
+        {"trait_type": "Layer-4", "value": "1"}
+      ],
       properties: {
         files: [{ uri: img, "type": "image/png" }],
         category: "image",
         creators: [{
+          address: "FS4BLiczU47tJnimNDpN4GVYaQcsJAtaYCympXrES7eS",
+          share: 100
         }]
       }
     }  
@@ -38,9 +47,4 @@ const imageDir = fs.readdirSync("./assets");imageDir.forEach(img => {
     let file = fs.writeFileSync(`./assets/${img.split(".")[0]}.json`,json)
   })
 
-
-
-
-1. Add collection name:
-2. magic 
   
